7f87dc2507c6c104451926285acb2fad13d89a79
Esi Meci <gowinmercy@gmail.com>
2023-12-07 04:51:59 -0800

    added litle minimal media quaries including dark mode

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// document.getElementById('year').textContent = new Date().getFullYear()
+
+const year = document.getElementById("year")
+const thisYear = new Date().getFullYear()
+year.textContent = thisYear
